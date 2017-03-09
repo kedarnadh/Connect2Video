7160ab526eda31eefec3401fb7a965d1dd08bbea
Vyshnavi Reddy <vyshnavireddy@Vyshnavis-MacBook-Air.local>
2017-03-09 15:40:25 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit{
+
+   //providing the title
+   title = "Connect2Video";
+   //peer is given var in the typing.d.ts
+   //declare the peer
+   //declare the peerid
+   //connectid is to generate the id for connecting
+  private peer;
+  private peerid;
+  private connectid;
+ //constructor for the app component
+  constructor(){
+
+  }
+//executes on initialisation
+  ngOnInit(){
+    //provide the generated key
+    this.peer = new Peer({'key': '4l00hr50y5app66r'});
+    //setting a timeout of 3sec to display the id
+    setTimeout( function() {
+      //connectid will be given your id from the server
+      this.connectid = this.peer.id;
+    },3000);
+    
+
+    
+      //inorder to receive the data
+      this.peer.on('connection', function(conn) {
+       conn.on('data', function(data){
+       // Will print 'Hey!'
+       console.log(data);
+  });
+});
+  // } 
+
+  }
+  //inorder to make the connection call
+  connect(){
+    var connection = this.peer.connect(this.peerid);
+    connection.on('open', function(){
+    connection.send('Hey this world!');
+});
+
+  }
+
+ 
 }
---
+++ src/typings.d.ts
@@ -0,0 +1,9 @@
+declare var System: any;
+declare var module:{ id: string};
+declare var require: any;
+
+
+
+declare var Peer: any;
+declare var peerid: any;
+declare var connectid: any;
